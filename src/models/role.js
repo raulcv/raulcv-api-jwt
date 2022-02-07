0d22f54170f259c5cbd876b4df0a308c85e3419a
Raul <iraulcv@gmail.com>
2022-02-07 01:06:53 -0500

    Adding state column to Role model

--- src/models/role.js
+++ src/models/role.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 
 const roleSchema = new mongoose.Schema({
-    name: {type: String, required: true, lowercase: true},
-    description: String
+    name: { type: String, required: true, lowercase: true },
+    description: String,
+    state: { type: String, uppercase: true, default: "A" },
+    createdat: { type: Date, lowercase: true, default: new Date() }
 })
 roleSchema.plugin(mongoosePaginate)
 const Role = mongoose.model("role", roleSchema);
