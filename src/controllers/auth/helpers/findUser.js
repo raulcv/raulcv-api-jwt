ce251722ec10ef75f09d6f1c7dbc5f478a08cce3
Raul <iraulcv@gmail.com>
2022-02-16 15:43:53 -0500

    reset password

---
+++ src/controllers/auth/helpers/findUser.js
@@ -0,0 +1,22 @@
+const User = require('../../../models/user')
+const { itemNotFound } = require('../../../middlewares/utils')
+
+/**
+ * Finds user by email
+ * @param {string} email - user´s email
+ */
+const findUser = (email = '') => {
+    return new Promise((resolve, reject) => {
+        User.findOne({ email }, 'password loginAttempts blockExpires name email role verified verification', async (err, item) => {
+            try {
+                await itemNotFound(err, item, 'USER_DOES_NOT_EXIST')
+                resolve(item)
+            } catch (error) {
+                reject(error)
+            }
+        }
+        )
+    })
+}
+
+module.exports = { findUser }
