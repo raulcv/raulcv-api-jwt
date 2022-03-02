11e04a2c647cf07e6793dd37023bedcce1a2d6b9
Raul <iraulcv@gmail.com>
2022-03-02 03:19:08 -0500

    Server cinfig modified to testing

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -14,9 +14,13 @@ const dev = {
 
 const test = {
     app: {
-        port: 3000
+        host: process.env.APP_HOST || 'localhost',
+        port: process.env.APP_PORT || 8081
     },
     db: {
+        host: process.env.DB_HOST + '://' || 'localhost',
+        port: process.env.DB_USER_NAME + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_CLUSTER + '.' + process.env.DB_PORT || 27017,
+        name: '/' + process.env.DB_NAME + '?retryWrites=true&w=majority' || 'db'
     }
 };
 
